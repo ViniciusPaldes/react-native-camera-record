5b4820e20224fa9c73bfe382cc8f5085b9cfce32
Vinicius Dias Paldês <vinicius.paldes@ctis.com.br>
2020-04-25 13:27:34 -0300

    Fix Multidex for Android Devices

--- android/app/src/main/java/com/camerarecord/MainApplication.java
+++ android/app/src/main/java/com/camerarecord/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.multidex.MultiDex;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -45,6 +46,12 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+
+  @Override
+  protected void attachBaseContext(Context base) {
+     super.attachBaseContext(base);
+     MultiDex.install(this);
   }
 
   /**
